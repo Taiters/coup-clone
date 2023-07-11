d42bfb12e15572c23c51d3376e2b73e825c2c793
Daniel Tait <dantait91@gmail.com>
2023-07-11 21:48:58 +0000

    Playing with models

--- app.py
+++ app.py
@@ -1,28 +1,58 @@
-import sqlite3
+import random
+import string
 
-from flask import Flask
+from uuid import uuid4
+from flask import Flask, jsonify, session
 from flask import g
+from sqlalchemy import select
 
 from coup_clone.database import db_session
 from coup_clone.database import db_session
-from coup_clone.models import User
+from coup_clone.models import Game, Player
 
 app = Flask(__name__)
+
+app.secret_key = b'<TEST:OH NO PLEASE DONT SCRAPE ME>'
+
+def get_session_id() -> str:
+    if 'session_id' not in session:
+        session['session_id'] = str(uuid4())
+    return session['session_id']
+
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
 
-@app.route("/")
-def hello_world():
-    users = User.query.all()
-    user_items = ''.join([f'<li>{u}</li>' for u in users])
-    return f'<ul>{user_items}</ul>'
+@app.route("/games")
+def games():
+    games = db_session.scalars(select(Game)).all()
+    return jsonify(
+        games=[g.id for g in games]
+    )
 
-@app.route("/add/<string:name>")
-def add_user(name):
-    u = User(name)
-    db_session.add(u)
+@app.route("/game/<string:game_id>")
+def game(game_id: str):
+    game = db_session.get(Game, game_id)
+    return jsonify(game_id=game.id, players=[{
+        "id": p.id,
+        "name": p.name,
+        "session_id": p.session_id,
+    } for p in game.players])
+
+@app.route("/game/<string:game_id>/join/<string:name>")
+def join_game(game_id: str, name: str):
+    game = db_session.get(Game, game_id)
+    player = Player(name=name, game_id=game_id, session_id=get_session_id())
+    db_session.add(player)
     db_session.commit()
-    return f'<p>Added {u}</p>'
+    return jsonify(player_id=player.id, name=player.name, game_id=player.game_id, session_id=player.session_id)
+
+@app.route("/game/create")
+def create_game():
+    game_id = ''.join([random.choice(string.ascii_lowercase) for _ in range(6)])
+    game = Game(id=game_id)
+    db_session.add(game)
+    db_session.commit()
+    return jsonify(game_id=game.id)
 
--- coup_clone/models.py
+++ coup_clone/models.py
@@ -1,13 +1,27 @@
-from sqlalchemy import Column, Integer, String
+from __future__ import annotations
+from typing import List
+
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
 from coup_clone.database import Base
 
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
 
-    def __init__(self, name=None):
-        self.name = name
+
+class Game(Base):
+    __tablename__ = 'games'
+    id: Mapped[str] = mapped_column(String(16), primary_key=True)
+    players: Mapped[List["Player"]] = relationship()
+
+
+class Player(Base):
+    __tablename__ = 'players'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+    game_id: Mapped[int] = mapped_column(ForeignKey("games.id"))
+    session_id: Mapped[str] = mapped_column(String(36))
 
     def __str__(self):
         return f'User: {self.name!r}'
