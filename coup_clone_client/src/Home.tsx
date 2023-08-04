9e9dcf2342cc4f3165d5bfb587122f42846055ff
Daniel Tait <dantait91@gmail.com>
2023-08-04 06:27:32 +0000

    Adding some buttons

--- coup_clone_client/src/Home.tsx
+++ coup_clone_client/src/Home.tsx
@@ -1,4 +1,5 @@
 import { useSubmit } from "react-router-dom";
+import styles from "./Home.module.css";
 
 function Home() {
     const submit = useSubmit();
@@ -9,16 +10,39 @@ function Home() {
         });
     }
     return (
-        <div>
-            <h1>Coup Clone</h1>
-            <div>
-                <button onClick={createGame}>Create Game</button>
-            </div>
-            <div>
-                <input type="text" />
-                <button>Join</button>
-            </div>
-        </div>
+        // <div>
+        //     <h1>Coup Clone</h1>
+        //     <div>
+        //         <button onClick={createGame}>Create Game</button>
+        //     </div>
+        //     <div>
+        //         <input type="text" />
+        //         <button>Join</button>
+        //     </div>
+        // </div>
+        <menu className={styles.actions}>
+            <li>
+                <button>Income</button>
+            </li>
+            <li>
+                <button>Foreign Aid</button>
+            </li>
+            <li>
+                <button>Tax</button>
+            </li>
+            <li>
+                <button>Steal</button>
+            </li>
+            <li>
+                <button>Assasinate</button>
+            </li>
+            <li>
+                <button>Exchnage</button>
+            </li>
+            <li>
+                <button>Coup</button>
+            </li>
+        </menu>
     );
 }
 
